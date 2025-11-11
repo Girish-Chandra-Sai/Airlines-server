6b9f2c0324f45a133da1148122f56c42d6319d9a
Girish <Girish-Chandra-Sai>
2025-11-11 21:00:06 +0530

    internal issues4

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/Users');
+const User = require('../models/User');
 
 router.get('/:userId/bookings', async (req, res) => {
   try {
